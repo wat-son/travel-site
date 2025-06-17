186c25c86ed4185edad1d4493638e6a98fc62deb
wat-son <kingkumi109@gmail.com>
2025-06-18 00:35:01 +0900

    20250618 bootを起動できるようにjavaファイルを追加

---
+++ src/main/java/com/example/travelsite/TravelSiteApplication.java
@@ -0,0 +1,11 @@
+package com.example.travelsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSiteApplication.class, args);
+    }
+}
